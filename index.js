d91814d98d5230387fc4bd16a0c06a97ef738d16
sacom14 <samir.comas1@gmail.com>
2023-11-12 23:01:39 +0100

    school server sprint8.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-
+const cors = require('cors'); //para poder hacer puts, y tal desde el cliente al servidor
 const app = express();
 
 app.use(function (req, res, next) {
@@ -9,6 +9,8 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', '*');
     next();
 });
+// Configura el middleware CORS para que peuda recibir solicitudes de POST, PUT, DELETE, UPDATE, etc.
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -39,7 +41,7 @@ app.get('/', (req, res) => {
 //endpoints
 //obtener lista de estudiantes
 app.get('/api/student', (req, res) => {
-    const query = `SELECT * FROM school.student`
+    const query = `SELECT *, DATE(birthdate_Student) FROM school.student`
     connection.query(query, (error, result) => {
         if (error) return console.log(error.message);
 
@@ -47,6 +49,24 @@ app.get('/api/student', (req, res) => {
             res.json(result);
         } else {
             res.json('No hay registros')
+        }
+    });
+});
+
+//get month of student
+app.get('/api/student/quantityMonth', (req, res) => {
+    const sql = `SELECT MONTHNAME(birthdate_student) AS month, COUNT(*) AS quantity
+    FROM student
+    GROUP BY MONTHNAME(birthdate_student), MONTH(birthdate_student)
+    ORDER BY MONTH(birthdate_student);`;
+
+    connection.query(sql, (error, result) => {
+        if (error) return console.log(error.message);
+
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.json('No hay registros');
         }
     });
 });
@@ -67,15 +87,16 @@ app.get('/api/student/:id', (req, res) => {
     })
 });
 
+
 //agregar estudiantes
 app.post('/api/student/add', (req, res) => {
     const student = {
-        name_Student: req.body.name_Student,
-        last_Name_Student: req.body.last_Name_Student,
-        class_Student: req.body.class_Student,
-        email_Student: req.body.email_Student,
-        dni_Student: req.body.dni_Student,
-        birthdate_Student: req.body.birthdate_Student,
+        name_Student: req.body.studentName,
+        last_Name_Student: req.body.studentLastName,
+        class_Student: req.body.studentClass,
+        email_Student: req.body.studentEmail,
+        dni_Student: req.body.studentDni,
+        birthdate_Student: req.body.studentBirthdate,
     }
 
     const query = `INSERT INTO school.student SET ?`
@@ -90,12 +111,13 @@ app.post('/api/student/add', (req, res) => {
 app.put('/api/student/update/:id', (req, res) => {
     const { id } = req.params;
     const { name_Student, last_Name_Student, class_Student, email_Student, dni_Student, birthdate_Student } = req.body;
-    
+    console.log('id', id);
+    console.log('body',req.body);
+
     const query = `UPDATE school.student SET name_Student ='${name_Student}', last_Name_Student ='${last_Name_Student}', class_Student ='${class_Student}', email_Student ='${email_Student}', dni_Student = '${dni_Student}', birthdate_Student = '${birthdate_Student}' WHERE id_Student ='${id}'`;
 
     connection.query(query, (error, result) => {
         if (error) return console.log(error.message);
-
         res.json(`Los datos del estudiante ${name_Student} ${last_Name_Student} se actualizaron correctamente`);
     });
 });
@@ -107,6 +129,8 @@ app.delete('/api/student/delete/:id', (req, res) => {
     connection.query(query, (error, result) => {
         if (error) return console.log(error.message);
 
-        res.json(`Los datos del estudiante se eliminaron correctamente`);
+        res.json(`Los datos de estudiante se eliminaron correctamente`);
     });
 });
+
+
